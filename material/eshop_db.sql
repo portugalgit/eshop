b628bafc54a87d1ab4719950e61c1f0855063f65
portugalgit <portugalgithub@gmail.com>
2025-07-15 17:28:56 +0100

    update script db

--- material/eshop_db.sql
+++ material/eshop_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 26-Jun-2025 às 00:29
+-- Tempo de geração: 15-Jul-2025 às 18:27
 -- Versão do servidor: 10.4.32-MariaDB
 -- versão do PHP: 8.0.30
 
@@ -24,6 +24,38 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `category` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `user_url` varchar(60) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `category` int(11) NOT NULL,
+  `price` double NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `image` varchar(500) NOT NULL,
+  `image2` varchar(500) NOT NULL,
+  `image3` varchar(500) NOT NULL,
+  `image4` varchar(500) NOT NULL,
+  `date` datetime NOT NULL,
+  `slag` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `users`
 --
 
@@ -32,14 +64,44 @@ CREATE TABLE `users` (
   `url_address` varchar(60) NOT NULL,
   `name` varchar(160) NOT NULL,
   `email` varchar(100) NOT NULL,
+  `fone` varchar(9) NOT NULL,
   `password` varchar(64) NOT NULL,
   `date` datetime NOT NULL,
   `rank` varchar(7) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Extraindo dados da tabela `users`
+--
+
+INSERT INTO `users` (`id`, `url_address`, `name`, `email`, `fone`, `password`, `date`, `rank`) VALUES
+(1, '8iqamvhbpbwh1d', 'Luisa Adelia', 'adelia@adelia.com', '934787764', 'f21ac14a5a8f4314f435631918a3d3534c4793b6', '2025-07-09 17:50:36', 'admin');
+
+--
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices para tabela `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category` (`category`);
+
+--
+-- Índices para tabela `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `price` (`price`),
+  ADD KEY `price_2` (`price`),
+  ADD KEY `slag` (`slag`),
+  ADD KEY `date` (`date`),
+  ADD KEY `description` (`description`),
+  ADD KEY `description_2` (`description`),
+  ADD KEY `category` (`category`),
+  ADD KEY `quantity` (`quantity`),
+  ADD KEY `user_url` (`user_url`);
 
 --
 -- Índices para tabela `users`
@@ -50,17 +112,30 @@ ALTER TABLE `users`
   ADD KEY `name` (`name`),
   ADD KEY `email` (`email`),
   ADD KEY `rank` (`rank`),
-  ADD KEY `date` (`date`);
+  ADD KEY `date` (`date`),
+  ADD KEY `fone` (`fone`);
 
 --
 -- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
+-- AUTO_INCREMENT de tabela `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de tabela `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
